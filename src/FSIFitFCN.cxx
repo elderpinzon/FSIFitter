92c61f88785e4df475a835e92dd2b8f9bff748de
elder <elder>
2016-05-30 06:22:38 +0000

    FSIFitFCN.* :    Add option to run either Octave or TMultiDimFit interpolation FSIChi2Grid.cxx: Move things around so octave interpolation works when reading grid

--- src/FSIFitFCN.cxx
+++ src/FSIFitFCN.cxx
@@ -10,17 +10,17 @@
 
 double FSIFitFCN::operator() (const std::vector<double> &x) const {
 
-  // Simply get the result of the interpolation evaluated at this grid point
-  double inter_chi2 = fFSIChi2Grid->GetInterpolatedGridPoint(x);
-  double splined_chi2 = fFSIChi2Grid->GetSplinedGridPoint(x);
-
   for(int i = 0; i < (int)x.size(); i++)
     std::cout << "\tx[" << i <<"]: "<<x[i];
+  std::cout << std::endl;
 
-  std::cout << "\tinter_chi2: " << inter_chi2 << " \t splined_chi2: " << splined_chi2 << std::endl;
-  
-  //return inter_chi2;
-  return splined_chi2;
-  
+  // Simply get the result of the desired interpolation evaluated at this grid point
+  if(kMultiDimFit)  return fFSIChi2Grid->GetInterpolatedGridPoint(x);
+  else if(kOctave)  return fFSIChi2Grid->GetSplinedGridPoint(x);
+  else{
+    std::cout << "FSIFitFCN: Must select either Octave or TMultiDimFit!" << std::endl;
+    std::exit(-1);
+  }
+
 }
 
