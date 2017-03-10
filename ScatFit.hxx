9409bd25d9d5c6e012e1b47910aa5bea4409aad7
elder <elder>
2017-03-10 01:17:42 +0000

    Very long overdue update of the FSIFitter code.
    Lots of updates and restructuring to be listed here (sorry). Some new files with new classes and apps coming shortly

--- ScatFit.hxx
+++ ScatFit.hxx
@@ -8,25 +8,47 @@
 #include "TFitterMinuit.h"
 #include "TMinuit.h"
 #include "TSystem.h"
+#include "TSystemDirectory.h"
 #include "TROOT.h"
 #include "TMath.h"
-#include "Math/Minimizer.h"
-#include "Math/Factory.h"
+#include "TCanvas.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TF1.h"
+#include "TPaveText.h"
+#include "TStopwatch.h"
+#include "Minuit2/Minuit2Minimizer.h"
+#include "Math/Functor.h"
+
+// #include "Math/Minimizer.h"
+// #include "Math/Factory.h"
 #include "Math/Functor.h"
 #include "Math/IFunction.h"
-#include "Math/Util.h"
+// #include "Math/Util.h"
 
 
 #include "ExternalDataSet.hxx"
 #include "FSIParameterScan.hxx"
 #include "FSIFitFCN.hxx"
+#include "FSIFitFCNNorm.hxx"
 #include "FSIChi2Grid.hxx"
+#include "FSIChi2GridNorm.hxx"
+#include "FSIFitterUtils.hxx"
+#include "ThrowParms.hxx"
+#include "TN032Envelopes.hxx"
+#include "ModelPrediction.hxx"
 
 // Running options
+bool kBuildGridFromScratch = true; //default option
 bool kUseInputGrid = false;
-bool kBuildGridFromScratch = false;
+bool kUseInputMultiDimFit = false;
 bool nuclFit=0;
 bool kOctave = false;
+int kPiece = -1;
+int kTotalPieces = -1;
+int dataFit = 0; // 0: {C}, 1: {C,Al,O}, 2:{C,O,Al,Fe,Cu,Pb}
+int nFitPars = 6;
+bool fitNorm = false;
 
 // Input file with MC scan
 std::string inputfileScan="";
@@ -39,6 +61,65 @@ std::string outputfileFit ="output/fit_result.root";
 
 // Vector of data sets for easier manipulation
 std::vector< ExternalDataSet::ExternalDataSet > AllDataSets;
+std::vector< ExternalDataSet::ExternalDataSet > NonDUETDataSets;
+int nDOF = 0;
 
 // FSIChi2Grid object
-FSIChi2Grid::FSIChi2Grid aFSIChi2Grid;
+FSIChi2Grid::FSIChi2Grid *aFSIChi2Grid;
+FSIChi2GridNorm *aFSIChi2GridNorm;
+
+// Clock for time-keeping
+TStopwatch stopwatch;
+
+//***********************************************************************************
+// Define list of data sets to be added and compile them as ExternalDataSets
+// objects in a vector to be used later
+//***********************************************************************************
+void AddDataSets(){
+  std::vector< std::string > DataSetsToBeAdded;
+  if (nuclFit){
+    std::cout<<"Nucleon Fit data---"<<std::endl;
+    DataSetsToBeAdded = {
+              // "c_reac_p_ch55",
+              "c_reac_p_mi54",
+              "c_reac_p_re72",
+              "c_tot_p_ca54",
+              "c_tot_p_ja78",
+              "c_tot_p_ma53",
+              "c_tot_p_sc79",
+              "c_elas_p_147",
+              "c_elas_p_131",
+              "c_elas_p_58",
+              "c_elas_p_33",
+              "c_elas_p_126",
+              "c_elas_p_24",
+              "c_elas_p_102",
+              "c_elas_p_124"
+            };
+  }
+}
+
+//***********************************************************************************
+// Alternative (more automatic) way of adding datasets
+//***********************************************************************************
+void AddDataSets(TString Nuclei){
+
+  TSystemDirectory dir("pion_data", "pion_data");
+  TList *files = dir.GetListOfFiles();
+  if (files) {
+    TSystemFile *file;
+    TString fname;
+    TIter next(files);
+    while ((file=(TSystemFile*)next())) {
+      fname = file->GetName();
+      if (!file->IsDirectory() && fname.EndsWith(".csv") && fname.BeginsWith(Nuclei)) {
+	fname = fname.ReplaceAll(".csv","");
+	std::cout << "\nDataset to be added: " << fname << std::endl;
+	ExternalDataSet::ExternalDataSet fDataSet(fname);
+	AllDataSets.push_back(fDataSet);
+	if(!(fname.Contains("DUET"))) NonDUETDataSets.push_back(fDataSet);
+	else nDOF += 10;
+      }
+    }
+  }
+}
--- src/FSIFitFCN.hxx
+++ src/FSIFitFCN.hxx
@@ -19,16 +19,20 @@ class FSIFitFCN : public ROOT::Minuit2::FCNBase {
 
   FSIFitFCN(){};
   ~FSIFitFCN(){};
-  FSIFitFCN(FSIChi2Grid *aFSIChi2Grid, bool fnuclFit, bool fOctave, bool fMultiDimFit);
-  void UseOctaveInterpolation(){ kOctave = true; kMultiDimFit = false;};
-  void UseTMultiDimFitInterpolation() {kMultiDimFit = true; kOctave = false;};
+  FSIFitFCN(FSIChi2Grid *aFSIChi2Grid);
+  void SetOctaveInterpolation(){ kOctave = true; kMultiDimFit = false;};
+  void SetMultiDimFitInterpolation() {kMultiDimFit = true; kOctave = false;};
   double operator() (const std::vector<double> & x) const;
+  double operator() (const double * par) const;
   double Up() const {return 1.;}
+  void SetScalingFactor(Double_t factor){scalingFactor = factor;};
+  void SetNPARS(Int_t NPARS){_NPARS = NPARS;};
 
  private:
   
   FSIChi2Grid *fFSIChi2Grid;
-  bool nuclFit;
+  Double_t scalingFactor;
+  Int_t _NPARS;
   bool kOctave;
   bool kMultiDimFit;
   
