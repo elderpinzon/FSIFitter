094bc80295fb784c05a8fa9741c3294669d8868c
elder <elder>
2017-04-29 15:39:41 +0000

    Also read in model predictions as TGraphs Small notation change for nuclei

--- src/ModelPrediction.hxx
+++ src/ModelPrediction.hxx
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <TFile.h>
 #include <TH1.h>
+#include <TGraph.h>
 #include <TCanvas.h>
 #include <TROOT.h>
 #include <TLegend.h>
@@ -15,7 +16,8 @@ private:
 
   TString ModelName;
   TFile *inFile;
-  TH1F* histo;
+  TH1D* histo;
+  TGraph* graph;
   Int_t kColor;
   bool kGeVtoMeV;
 
@@ -25,11 +27,12 @@ public:
   ModelPrediction(TString FileName);
   ~ModelPrediction();
   TString GetModelName(){return ModelName;};
-  TH1F* GetCrossSection(TString nuclei, TString type, TString xsec);
+  TH1D* GetCrossSection(TString nuclei, TString type, TString xsec);
+  TGraph* GetCrossSectionGraph(TString nuclei, TString type, TString xsec);
   Double_t GetCrossSectionValue(TString nuclei, TString type, TString xsec,Double_t momentum);
   void SetLineColor(Int_t color){kColor = color;};
   void SetGeVtoMeV(){kGeVtoMeV = true;};
-  TH1F* GeVtoMeV();
+  TH1D* GeVtoMeV();
 };
 
 #endif
